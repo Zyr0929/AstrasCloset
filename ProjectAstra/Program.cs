8f9f9fd4da60760d83a6b1c0f897693ff0f26aac
Ed Cyruz Mananquil <edcyruz54@gmail.com>
2026-06-07 19:18:54 +0800

    Latest

--- ProjectAstra/Program.cs
+++ ProjectAstra/Program.cs
@@ -2,22 +2,18 @@ using Microsoft.EntityFrameworkCore;
 using ProjectAstra.Models;
 
 
-// Add this block:
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
