5c23fef213e631ce8ae385c51af0cb28bcdc0f40
ZokDevz <163521582+ZokDevz@users.noreply.github.com>
2026-06-08 14:39:37 +0800

    New Razor Store

---
+++ ProjectAstra/Pages/Store.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ProjectAstra.Pages
+{
+    public class StoreModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
